3db34264f00bffaf253d3f358c6b1bb43c5e2a02
Yo Iwasaki <asahikawa009@gmail.com>
2023-10-06 01:36:05 +0900

    Write operation is complete

--- soft/greenpak_ctr.c
+++ soft/greenpak_ctr.c
@@ -16,16 +16,19 @@
 
 #define NVM_CONFIG 0x02
 #define EEPROM_CONFIG 0x03
-#define MAX_LINE_LENGTH 256
+
 
 int8_t i2c_read(
     uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t length);
 int8_t i2c_write(
-    uint8_t dev_addr, uint8_t reg_addr, const uint8_t *data, size_t* length);
+    uint8_t dev_addr, uint8_t reg_addr, const uint8_t *data, uint16_t length);
+int8_t i2c_write_for_erase(
+    uint8_t dev_addr, uint8_t reg_addr, const uint8_t* data, uint16_t length);
 int readchip(char *NVMorEEPROM);
 void erasechip(char *NVMorEEPROM);
 int writechip(char* NVMorEEPROM, char* csv_path);
-uint8_t* read_csv(const char* filename, size_t* array_size);
+uint8_t* read_csv(const char* filename, uint16_t* array_size);
+uint8_t** convert_to_2d_array(const uint8_t* input);
 uint8_t soft_reset();
 
 
@@ -33,7 +36,6 @@ uint8_t soft_reset();
 
 uint8_t slave_address = 0x01; //greenpakの初期値は0x01
 
-uint8_t data_array[16][16] = {};
 static const char *dev_name = "/dev/i2c-3";
 size_t i, j;
 
@@ -81,10 +83,52 @@ int8_t i2c_read(
  * @param[in] data 書き込むデータの格納場所を指すポインタ.
  * @param[in] length 書き込むデータの長さ.
  */
-int8_t i2c_write(
-    uint8_t dev_addr, uint8_t reg_addr, const uint8_t *data, size_t* length)
+int8_t i2c_write_for_erase(
+    uint8_t dev_addr, uint8_t reg_addr, const uint8_t* data, uint16_t length)
 {
-  int ret;
+  /* I2Cデバイスをオープンする. */
+  int32_t fd = open(dev_name, O_RDWR);
+
+  if (fd == -1)
+  {
+    fprintf(stderr, "i2c_write: failed to open: %s \n", strerror(errno));
+    return -1;
+  }
+
+  /* I2C-Write用のバッファを準備する. (lengthに+1しているのはレジスタアドレスも送信する必要があるため)*/
+  uint8_t *buffer = (uint8_t *)malloc(length + 1);
+  if (buffer == NULL)
+  {
+    fprintf(stderr, "i2c_write: failed to memory allocate\n");
+    close(fd);
+    return -1;
+  }
+  buffer[0] = reg_addr;             /* 1バイト目にレジスタアドレスをセット. */
+  memcpy(&buffer[1], data, length); /* 2バイト目以降にデータをセット. */
+
+  /* I2C-Writeメッセージを作成する.(lengthに+1しているのはレジスタアドレスも送信する必要があるため) */
+  struct i2c_msg message = {dev_addr, 0, length + 1, buffer};
+  struct i2c_rdwr_ioctl_data ioctl_data = {&message, 1};
+
+  /* I2C-Writeを行う. */
+  ioctl(fd, I2C_RDWR, &ioctl_data);
+
+  free(buffer);
+  close(fd);
+  return 0;
+}
+
+
+
+/*! I2Cスレーブデバイスにデータを書き込む.
+ * @param[in] dev_addr デバイスアドレス.
+ * @param[in] reg_addr レジスタアドレス.
+ * @param[in] data 書き込むデータの格納場所を指すポインタ.
+ * @param[in] length 書き込むデータの長さ.
+ */
+int8_t i2c_write(
+    uint8_t dev_addr, uint8_t reg_addr, const uint8_t* data, uint16_t length)
+{
   /* I2Cデバイスをオープンする. */
   int32_t fd = open(dev_name, O_RDWR);
   
@@ -95,7 +139,7 @@ int8_t i2c_write(
   }
 
   /* I2C-Write用のバッファを準備する. (lengthに+1しているのはレジスタアドレスも送信する必要があるため)*/
-  uint8_t *buffer = (uint8_t *)malloc(*length + 1);
+  uint8_t *buffer = (uint8_t *)malloc(length + 1);
   if (buffer == NULL)
   {
     fprintf(stderr, "i2c_write: failed to memory allocate\n");
@@ -103,10 +147,10 @@ int8_t i2c_write(
     return -1;
   }
   buffer[0] = reg_addr;             /* 1バイト目にレジスタアドレスをセット. */
-  memcpy(&buffer[1], data, *length); /* 2バイト目以降にデータをセット. */
+  memcpy(&buffer[1], data, length); /* 2バイト目以降にデータをセット. */
 
   /* I2C-Writeメッセージを作成する.(lengthに+1しているのはレジスタアドレスも送信する必要があるため) */
-  struct i2c_msg message = {dev_addr, 0, *length + 1, buffer};
+  struct i2c_msg message = {dev_addr, 0, length + 1, buffer};
   struct i2c_rdwr_ioctl_data ioctl_data = {&message, 1};
 
   /* I2C-Writeを行う. */
@@ -129,12 +173,13 @@ int8_t i2c_write(
 int readchip(char *NVMorEEPROM)
 {
   uint8_t control_code = slave_address << 3;
+  uint8_t data_array[16][16] = {};
 
-  if (strcmp(NVMorEEPROM, "NVM"))
+  if (strcmp(NVMorEEPROM, "NVM") == 0)
   {
     control_code |= NVM_CONFIG;
   }
-  else if (strcmp(NVMorEEPROM, "EEPROM"))
+  else if (strcmp(NVMorEEPROM, "EEPROM") == 0)
   {
     control_code |= EEPROM_CONFIG;
   }else{
@@ -149,45 +194,10 @@ int readchip(char *NVMorEEPROM)
       i2c_read(control_code, ((i << 4) + j), data_array[i] + j, 1);
        //i:上位4bit j:下位4bit data_array[i]+j:データを格納する配列のポインタ 
       fprintf(stderr, "%x :", ((i << 4) + j));
-      fprintf("%x \n", data_array[i][j]);
+      fprintf(stderr, "%x \n", data_array[i][j]);
     }
   }
   return 0;
-}
-
-/*! NVM領域かEEPROM領域いずれかの内容を消去する
- * @param[in] NVMorEEPROM 対象を示す変数
- */
-void erasechip(char *NVMorEEPROM)
-{
-  uint8_t control_code = slave_address << 3;
-  uint8_t addressForAckPolling = control_code;
-  size_t length = 1;
-  uint8_t tmp;
-
-  for (i = 0; i < 16; i++)
-  {
-    fprintf(stderr, "erasing page:%x", i);
-
-    if (strcmp(NVMorEEPROM,"NVM") )
-    {
-      fprintf(stderr, "NVM");
-      control_code = 0x00;
-      control_code |= NVM_CONFIG; // adress上位3bitがそれぞれNVM、eeprom、registerに紐づいているため( slave adressはcontorol_code[4bit] + adress上位3bit の8bit)
-      tmp = 0x80 | i;
-      i2c_write(control_code, 0xE3, &tmp, &length);
-    }
-    else if (strcmp(NVMorEEPROM,"EEPROM"))
-    {
-      fprintf(stderr, "EEPROM");
-      tmp = 0x90 | i;
-      i2c_write(control_code, 0xE3, &tmp , &length);
-    }
-    usleep(40000); //消去時間がmax 20msらしいが念のため20ms待つことにする
-  }
-  
-  slave_address = 0x00; // 上の消去処理でslaveアドレスを決めているレジスタ"0xCA"も消してしまっているから
-  
 }
 
 
@@ -198,13 +208,19 @@ void erasechip(char *NVMorEEPROM)
 int writechip(char* NVMorEEPROM, char* csv_path)
 {
   uint8_t control_code;
-  size_t array_size;
+  uint16_t array_size;
+  printf("csv file read\n");
   uint8_t* data = read_csv(csv_path, &array_size);
-
+  printf("convert 2d array \n");
+  uint8_t** data_array = convert_to_2d_array(data);
+  if (!data_array) {
+    printf("Incorrect CSV file.\n");
+        return 1;
+  }
   
   if (strcmp(NVMorEEPROM, "NVM") == 0)
   {
-    erasechip(NVMorEEPROM);
+    //erasechip(NVMorEEPROM);
     control_code = slave_address << 3;
     control_code |= NVM_CONFIG;
   }
@@ -222,12 +238,26 @@ int writechip(char* NVMorEEPROM, char* csv_path)
 
   for (i = 0; i < 16; i++)
   {
-    for (j = 0; j < 16; j++)
-    {
-      i2c_write(control_code, ((i << 4) + j), data_array[i] + j, &array_size);
-       //i:上位4bit j:下位4bit data_array[i]+j:データを格納する配列のポインタ 
+    printf("Write the following 16 bytes\n");
+    printf("-------------\n");
+    for(j = 0 ; j < 16; j++){
+      printf("adress:%x, value:%x \n",((i << 4) + j), data_array[i][j]);
     }
+    printf("-------------\n");
+    printf("i2c_write \n");
+    i2c_write(control_code, (i << 4), data_array[i], array_size);
+    //i:上位4bit j:下位4bit data_array[i]+j:データを格納する配列のポインタ
+    usleep(60000);
+    if (i != 15 && j != 15)
+    {
+      printf("next page \n");
+      printf("\n");
+    }else{
+      printf("end write\n");
+    }
+
   }
+
   soft_reset();
   return 0;
 
@@ -235,13 +265,65 @@ int writechip(char* NVMorEEPROM, char* csv_path)
 }
 
 
+/*! NVM領域かEEPROM領域いずれかの内容を消去する
+ * @param[in] NVMorEEPROM 対象を示す変数
+ */
+void erasechip(char *NVMorEEPROM)
+{
+  uint8_t control_code = slave_address << 3; //3bit左シフトさせつつA10,A9,A8を0にして"ERSR register"のBlock Adressに設定している
+  uint8_t addressForAckPolling = control_code;
+  uint16_t length = 1;
+  uint8_t tmp;
 
+  for (i = 0; i < 16; i++)
+  {
+    fprintf(stderr, "erasing page:%x", i);
+
+    if (strcmp(NVMorEEPROM,"NVM") == 0)
+    {
+      fprintf(stderr, "NVM \n");
+      tmp = 0x80 | i;
+      i2c_write_for_erase(control_code, 0xE3, &tmp, length);
+    }
+    else if (strcmp(NVMorEEPROM,"EEPROM") == 0)
+    {
+      fprintf(stderr, "EEPROM");
+      tmp = 0x90 | i;
+      i2c_write_for_erase(control_code, 0xE3, &tmp , length);
+    }
+    usleep(40000); //消去時間がmax 20msらしいが念のため20ms待つことにする
+  }
+
+  slave_address = 0x00; // 上の消去処理でslaveアドレスを決めているレジスタ"0xCA"も消してしまっているから
+
+}
+
+uint8_t soft_reset(){
+  //レジスタ C8 [1601] (I2C リセット ビット)を「1」に設定する
+  uint8_t tmp;
+  uint16_t length = 1;
+  uint8_t control_code = slave_address << 3;
+  control_code |= NVM_CONFIG;
+  if(i2c_read(control_code, 0xC8, &tmp,1)){
+    printf("i2c communication failed. \n");
+    return 1;
+  }
+  tmp = tmp | 0x01;
+  if(i2c_write(control_code, 0xC8, &tmp , length)){
+    printf("Failed to write to [1601]bit \n");
+    return 1;
+  }
+  usleep(500000);
+
+}
+
+#define MAX_LINE_LENGTH 256
 /*! CSVファイルからuint8_tの配列を読み取る関数
  * @param[in] filename 読み込むcsvファイル名
  * @param[in,out] array_size データの入った配列のサイズが入る 
  */
 // CSVファイルからuint8_tの配列を読み取る関数
-uint8_t* read_csv(const char* filename, size_t* array_size) {
+uint8_t* read_csv(const char* filename, uint16_t* array_size) {
     FILE* file = fopen(filename, "r");
     
     if (!file) {
@@ -252,7 +334,7 @@ uint8_t* read_csv(const char* filename, size_t* array_size) {
     char line[MAX_LINE_LENGTH];
     uint8_t* data_array = NULL;
     size_t current_index = 0;
-    char *end = NULL;
+    char *end;
     
     while (fgets(line, sizeof(line), file) != NULL) {
         // 改行文字を削除
@@ -260,12 +342,12 @@ uint8_t* read_csv(const char* filename, size_t* array_size) {
         if (line_length > 0 && line[line_length - 1] == '\n') {
             line[line_length - 1] = '\0';
         }
-        
+        printf("%s\n",line);
         // 16進数文字列からuint8_tに変換して配列に格納
-        uint8_t num = (uint8_t)strtol(line, &end, 16);
-        if (end != NULL)
+        uint8_t num = (uint8_t)strtol(line, &end, 16);   //
+        if (*end != '\0')
         {
-            printf("Error! Contains invalid characters!");
+            printf("Error! Contains invalid characters!\n");
             return NULL;
         }
         data_array = realloc(data_array, (current_index + 1) * sizeof(uint8_t));
@@ -274,6 +356,7 @@ uint8_t* read_csv(const char* filename, size_t* array_size) {
             fclose(file);
             return NULL;
         }
+
         data_array[current_index] = num;
         current_index++;
     }
@@ -283,24 +366,44 @@ uint8_t* read_csv(const char* filename, size_t* array_size) {
     return data_array;
 }
 
-uint8_t soft_reset(){
-  //レジスタ C8 [1601] (I2C リセット ビット)を「1」に設定する
-  uint8_t tmp;
-  size_t length = 1;
-  uint8_t control_code = slave_address << 3;
-  control_code |= NVM_CONFIG;
-  if(i2c_read(control_code, 0xC8, &tmp,1)){
-    printf("i2c communication failed. \n");
-    return 1;
-  }
-  tmp = tmp | 0x01;
-  if(i2c_write(control_code, 0xC8, &tmp , &length)){
-    printf("Failed to write to [1601]bit \n");
-    return 1;
-  }
-  usleep(500000);
+#define INPUT_SIZE 256
+#define OUTPUT_ROWS 16
+#define OUTPUT_COLS 16
 
+// 1次元配列を2次元配列に変換する関数
+uint8_t** convert_to_2d_array(const uint8_t* input)
+{
+    uint8_t** output = (uint8_t**)malloc(OUTPUT_ROWS * sizeof(uint8_t*));
+    if (!output) {
+        perror("Memory allocation error");
+        return NULL;
+    }
+
+    for (i = 0; i < OUTPUT_ROWS; i++) {
+        output[i] = (uint8_t*)malloc(OUTPUT_COLS * sizeof(uint8_t));
+        if (!output[i]) {
+            perror("Memory allocation error");
+            for (int j = 0; j < i; j++) {
+                free(output[j]);
+            }
+            free(output);
+            return NULL;
+        }
+    }
+
+    size_t input_index = 0;
+    for (i = 0; i < OUTPUT_ROWS; i++) {
+        for (j = 0; j < OUTPUT_COLS; j++) {
+            output[i][j] = input[input_index];
+            input_index++;
+        }
+    }
+
+    return output;
 }
+
+
+
 
 
 
@@ -323,14 +426,14 @@ int main(int argc, char *argv[]) {
         printf("Option 1, NVM selected. \n");
         // Processing for the case when the first argument is "option1"
         if (strcmp(argv[3], "-r") == 0) {
-            printf("Option 1, Sub-option -r selected.\n");
+            printf(" Sub-option -r selected.\n");
             readchip(argv[2]);
 
         } else if (strcmp(argv[3], "-w") == 0) {
             printf("Option 1, Sub-option -w selected.\n");
             writechip(argv[2], argv[1]);
 
-        } else if (strcmp(argv[3], "e") == 0) {
+        } else if (strcmp(argv[3], "-e") == 0) {
             printf("Option 1, Sub-option -e selected.\n");
             erasechip(argv[2]);
 
